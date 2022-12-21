fd1c2c414de65b45c3576c870ac4391136faaa16
fatihyazicioglu <fy134388@gmail.com>
2022-12-21 22:16:07 +0100

    photo changed

--- src/Baner.jsx
+++ src/Baner.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import BanerImg from "./images/banerimg.jpg"
+import BanerImg1 from "./images/IMG_20221221_220434.jpg"
+
 
 function Baner() {
   return (
     <div className='baner' id='baner'>
-        <img src={BanerImg} alt="" /></div>
+        <img src={BanerImg1} alt="" />
+        
+        </div>
+       
   )
 }
 
